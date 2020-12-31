a42c0e590d233e9261f0a2389fc6fb5c3d811d4f
kalabovanikola <nikola@kalabova.eu>
2020-12-31 22:57:23 +0100

    labels changed

--- scripts/get_columns1.sh
+++ scripts/get_columns1.sh
@@ -1,6 +1,6 @@
 for var in "$@"
 do
 	substri=$(echo $var| cut -d'/' -f 2)
-	cat $var | cut --complement -f1 | sed '/^[[:space:]]*$/d' | cut -f3,9,11 |  awk '{print substr($0,0,2),$2,$3}' | awk '$3 != "" { print }' | awk '$2 ~ /2020/{print}' > "./cutted/cutted_$substri"
+	cat $var | cut --complement -f1 | sed '/^[[:space:]]*$/d' | cut -f3,9,11 |  awk '{print substr($0,0,2),$2,$3}' | awk '$3 != "" { print }' | awk '$2 ~ /2020/{print}' | awk '/po/ { $3 = "POZ" } 1' | awk '/ne/ { $3 = "NEG" } 1'> "./cutted/cutted_$substri"
 done
 
