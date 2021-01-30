b1c17e37a440f377fafba55f8553eebee0075183
Nikola Kalábová <nikola@kalabova.eu>
2021-01-30 14:02:26 +0100

    Delete by_date.sh

--- by_date.sh
+++
@@ -1,5 +0,0 @@
-bash by_date_scripts/by_date1.sh text_files/*
-bash by_date_scripts/by_date2.sh text_files2/*
-bash by_date_scripts/merge.sh by_date/*.*
-bash by_date_scripts/split.sh by_date/merged/merged2.txt
-bash by_date_scripts/join.sh
